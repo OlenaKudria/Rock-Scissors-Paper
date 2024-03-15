381081e528c0dff7956615659e163416148675d9
Olena Kudria <kudriaelenad@gmail.com>
2024-03-15 18:57:45 +0000

    Added hands

---
+++ Rock-Scissors-Paper/GameAssets.cs
@@ -0,0 +1,59 @@
+﻿namespace Rock_Scissors_Paper;
+
+public static class GameAssets
+{
+    public const string Paper = @"
+                                          _______
+                                     ---'    ________)
+                                                ______)
+                                               _______)
+                                              _______)
+                                     ---.__________)
+                                     ";
+    
+    public const string MirroredPaper = @"
+                            _______
+                       (________     '---
+                      (______
+                      (_______
+                       (_______
+                         (_________.---
+                           ";
+
+    public const string Rock = @"
+                                        _______
+                                    ---'   ____)
+                                          (_____)
+                                          (_____)
+                                          (____)
+                                    ---.__(___)
+                                    ";
+    
+    public const string MirroredRock= @"
+                           _______
+                          (____   '---
+                         (_____)
+                         (_____)
+                          (____)
+                           (___)__.---
+                        ";
+
+    public const string Scissors = @"
+                                            _______
+                                        ---'   ____)
+                                                  ______)
+                                               __________)
+                                              (____)
+                                        ---.__(___)
+                                        ";
+    
+    public const string MirroredScissors = @"
+                                _______
+                               (____   '---   
+                          (______
+                         (__________
+                               (____)
+                                (___)__.---
+                                        ";
+    
+}
